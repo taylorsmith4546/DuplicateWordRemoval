28079e277bdbe4430327f2ac483dc268f054aed8
Taylor Smith <taylorsmith4546@gmail.com>
2016-04-28 08:45:34 -0700

    second commit

--- DuplicateWordRemoval/DuplicateWordRemoval/Program.cs
+++ DuplicateWordRemoval/DuplicateWordRemoval/Program.cs
@@ -8,8 +8,27 @@ namespace DuplicateWordRemoval
 {
     class Program
     {
+        public static List<string> sentenceList;
+
         static void Main(string[] args)
         {
+            string sentence = "";
+            sentenceList = new List<string>();
+
+            Console.WriteLine("Please enter a sentence and I'll display DISTINCT words");
+            sentence = Console.ReadLine();
+
+            sentenceList = sentence.ToLower().Split().Distinct().ToList();
+
+            var query = from word in sentenceList
+                        orderby word
+                        select word;
+
+            foreach (var element in query)
+                Console.WriteLine(element);
+
+            Console.ReadLine();
+
         }
     }
 }
